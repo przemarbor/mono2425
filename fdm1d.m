96d2174a2e03823672eafd15b7ff5bd0be733c57
przemarbor <marbor@prz.edu.pl>
2024-10-16 15:06:48 +0200

    Zaimplementowane: rownanie Poissona + wb Dir i Neu
    TODO:
    nieuwzglednione war. brzeg. Neu.
    - bledy w linijkach 50 i 61
      - aktualizacja wektora b o war. brzeg.
      - b(1) a nie b(n) !!!

--- fdm1d.m
+++ fdm1d.m
@@ -11,21 +11,24 @@ clc
 aa = 0 ;
 bb = 1 ;
 
-n = 5 ; % liczba wezlow siatki
+n = 11 ; % liczba wezlow siatki
 x = linspace(aa,bb,n) ;
 dx = x(2) - x(1) ;
 h = dx ;
 
+f_fh = @(x) 1*ones(size(x)) ;
+
 %% Warunki brzegowe
-wbLt = 'D' ;
+wbLt = 'N' ;
 wbRt = 'D' ;
 
-wbLw = 30 ;
-wbRw = 15 ;
+wbLw =  1 ;
+wbRw =  2 ;
 
 % inicjalizacja zmiennych potrzebnych w trakcie obliczen
 A = zeros(n, n) ;
 b = zeros(n, 1) ;
+b = f_fh( x(:) ) ;
 
 % PROCESSING
 
@@ -42,7 +45,9 @@ if wbLt == 'D'
   A(1,1) = 1 ;
   b(1)   = wbLw ;
 elseif wbLt == 'N'
-  TODO
+  A(1,1)   =  2/h^2 ;
+  A(1,2)   = -2/h^2 ;
+  b(n)     = wbLw * 2* h/ h^2 ;
 else
   error('Bledny typ war. brzeg.')
 end
@@ -51,18 +56,39 @@ if wbRt == 'D'
   A(n,n) = 1 ;
   b(n)   = wbRw ;
 elseif wbRt == 'N'
-  TODO
+  A(n,n)   = 2/h^2 ;
+  A(n,n-1) = -2/h^2 ;
+  b(n)     = wbRw * 2* h/ h^2 ;
 else
   error('Bledny typ war. brzeg.')
 end
 
-T = A\b
+T = A\b ;
 
 % POSTPROCESSING
 
 figure, plot(x, T, 'ro')
 hold on
-plot(x, 30 - 15*x, 'b')
+% plot(x, 30 - 15*x, 'b')
+% plot(x, 100 - 300*x, 'b')
+% plot(x, -0.5 * x.^2 + 0.5 * x, 'b')
 hold off
 
+% sprawdzenie spelnienia warunkow brzeg.
+
+if (wbRt == 'D')
+  disp(['Rozw. przyblizone ' num2str(T(end)) ])
+elseif (wbRt == 'N')
+  disp(['Rozw. przyblizone ' num2str( (T(end) - T(end-1))/h  ) ])
+else
+  error('Zly typ WB')
+end
+
+if (wbLt == 'D')
+  disp(['Rozw. przyblizone ' num2str(T(1)) ])
+elseif (wbLt == 'N')
+  disp(['Rozw. przyblizone ' num2str( -(T(2) - T(1))/h  ) ])
+else
+  error('Zly typ WB')
+end
 
